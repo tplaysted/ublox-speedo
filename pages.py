fecbd31c2dd717cbb7ece9b79abf172f0c7b9b49
Tom P <t.playsted@gmail.com>
2026-01-07 20:44:07 +1100

    Change to monospace font and add Default page

--- pages.py
+++ pages.py
@@ -1,14 +1,13 @@
+import math
+
 from color_setup import ssd
 from gui.core.nanogui import refresh
+from gui.core.writer import Writer  # Renders color text
+from gui.fonts import mono10
 from gui.widgets.dial import Dial, Pointer
 from gui.widgets.label import Label  # Import any widgets you plan to use
 
 refresh(ssd, True)  # Initialise and clear display.
-
-import math
-
-from gui.core.writer import Writer  # Renders color text
-from gui.fonts import arial10
 
 
 class Quality:
@@ -17,7 +16,7 @@ class Quality:
         Writer.set_textpos(ssd, 0, 0)  # In case previous tests have altered it
 
         # Instantiate any Writers to be used (one for each font)
-        self.wri = Writer(ssd, arial10, verbose=False)  # Monochrome display uses Writer
+        self.wri = Writer(ssd, mono10, verbose=False)  # Monochrome display uses Writer
         self.wri.set_clip(True, True, False)
         self.siv_lbl = Label(self.wri, 2, 68, 59)
         self.pre_lbl = Label(self.wri, 26, 68, 59)
@@ -43,7 +42,7 @@ class Quality:
         x, y = self.get_sat_xy(data)
         lbl.row = y - 3
         lbl.col = x - 3
-        val = "-" if data[2] is None else int(data[2] / 10.0)
+        val = "?" if data[2] is None else int(data[2] / 10.0)
         lbl.value(f"{val}", invert=True)
 
     def update_sat_labels(self, gps):
@@ -65,7 +64,7 @@ class Quality:
             if svid in self.sat_labels:  # update label
                 lbl = self.sat_labels[svid]
             else:  # add label
-                lbl = Label(self.wri, 0, 0, 10)
+                lbl = Label(self.wri, 0, 0, 6)
                 self.sat_labels[svid] = lbl
 
             self.draw_sat(lbl, data)
@@ -77,17 +76,51 @@ class Quality:
     def load(self, gps):  # to be called when changing to this page
         refresh(ssd, True)  # Initialise and clear display.
         self.circles()
-        self.siv_lbl.value(f"In view: {gps.satellites_in_view}")
-        Label(self.wri, 16, 68, "Horiz. PRE")
-        self.pre_lbl.value(f" = {math.sqrt(gps.std_lat**2 + gps.std_lon**2):.2f} m")
-        Label(self.wri, 40, 68, "Alt. error")
-        self.alt_lbl.value(f" = {gps.std_alt:.2f} m")
+        self.siv_lbl.value(f"In view:{gps.satellites_in_view}")
+        Label(self.wri, 16, 68, "Horiz.PRE")
+        self.pre_lbl.value(f" ={math.sqrt(gps.std_lat**2 + gps.std_lon**2):.2f} m")
+        Label(self.wri, 40, 68, "Alt.error")
+        self.alt_lbl.value(f" ={gps.std_alt:.2f} m")
 
         refresh(ssd)
 
     def refresh(self, gps):  # to be called at around 10 Hz
-        self.siv_lbl.value(f"In view: {gps.satellites_in_view}")
-        self.pre_lbl.value(f" = {math.sqrt(gps.std_lat**2 + gps.std_lon**2):.2f} m")
-        self.alt_lbl.value(f" = {gps.std_alt:.2f} m")
+        self.siv_lbl.value(f"In view:{gps.satellites_in_view}")
+        self.pre_lbl.value(f" ={math.sqrt(gps.std_lat**2 + gps.std_lon**2):.2f} m")
+        self.alt_lbl.value(f" ={gps.std_alt:.2f} m")
         self.update_sat_labels(gps)
+        refresh(ssd)
+
+
+class Default:
+    def __init__(self):
+        # Writer for gui elements
+        Writer.set_textpos(ssd, 0, 0)  # In case previous tests have altered it
+
+        # Instantiate any Writers to be used (one for each font)
+        self.wri = Writer(ssd, mono10, verbose=False)  # Monochrome display uses Writer
+        self.wri.set_clip(True, True, False)
+
+        self.date_lbl = Label(self.wri, 2, 2, 120)
+        self.time_lbl = Label(self.wri, 12, 2, 120)
+        self.lat_lbl = Label(self.wri, 32, 2, 120)
+        self.lon_lbl = Label(self.wri, 42, 2, 120)
+        self.alt_lbl = Label(self.wri, 52, 2, 120)
+
+    def load(self, gps):  # to be called when changing to this page
+        refresh(ssd, True)  # Initialise and clear display.
+        self.date_lbl.value(f"{gps.date_string()}")
+        self.time_lbl.value(f"{gps.time_string()}")
+        self.lat_lbl.value(f"Lat. {gps.latitude_string()}")
+        self.lon_lbl.value(f"Lon. {gps.longitude_string()}")
+        self.alt_lbl.value(f"Alt. {gps.altitude:.1f} m")
+        refresh(ssd)
+
+    def refresh(self, gps):  # to be called at around 10 Hz
+        self.date_lbl.value(f"{gps.date_string()}")
+        self.time_lbl.value(f"{gps.time_string()}")
+        self.lat_lbl.value(f"Lat. {gps.latitude_string()}")
+        self.lon_lbl.value(f"Lon. {gps.longitude_string()}")
+        self.alt_lbl.value(f"Alt. {gps.altitude:.1f} m")
+
         refresh(ssd)
